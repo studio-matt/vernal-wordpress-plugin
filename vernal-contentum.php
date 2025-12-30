6da0cb313d736e91e440525d1e4ed18d34eb4580
“Matt” <matt@envoydesign.com>
2025-12-30 16:16:23 -0500

    Test deployment - attempt 2

--- vernal-contentum.php
+++ vernal-contentum.php
@@ -8,7 +8,7 @@
  * License: GPL v2 or later
  * Text Domain: vernal-contentum
  * 
- * Last updated: Testing deployment with IP address (50.6.198.220)
+ * Last updated: Testing deployment with IP address - attempt 2
  */
 
 // Exit if accessed directly
